1c04d1697b3f705681cab2d9df1755de68b6be4b
Nhan Doan <nhandev88@gmail.com>
2020-05-08 02:09:04 +0700

    Uninstall redundant packages

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-it('renders', () => {
-  const { asFragment } = render(<App />);
-  expect(asFragment().textContent).toBe('#Hubble30');
-});
