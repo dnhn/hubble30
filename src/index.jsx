e790033ac3da259c6fe65cbfa761cb0c1f0bbb4f
Nhan Doan <nhandev88@gmail.com>
2023-03-02 20:24:38 +0700

    Upgrade to React 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root'),
 );
